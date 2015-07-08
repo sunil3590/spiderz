7996faf121e9855c19399b970c3fb3e07c06d70e
Sunil D S <sunil3590@gmail.com>
2015-07-08 01:12:21 -0400

    resolve import warnings

--- src/main/java/edu/ncsu/spiderz/WikiCrawlerExplorerBolt.java
+++ src/main/java/edu/ncsu/spiderz/WikiCrawlerExplorerBolt.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
--- src/main/java/edu/ncsu/spiderz/WikiCrawlerTopology.java
+++ src/main/java/edu/ncsu/spiderz/WikiCrawlerTopology.java
@@ -1,35 +1,9 @@
 package edu.ncsu.spiderz;
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
-import storm.trident.TridentTopology;
-import storm.trident.testing.Split;
-
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
-import backtype.storm.LocalDRPC;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
-import backtype.storm.utils.Utils;
-
-import com.google.common.collect.Lists;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 public class WikiCrawlerTopology {
 
